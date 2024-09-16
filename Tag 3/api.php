7beeada08dc9b172b67cec3f9f17c248874cd7e3
Linus Lind <linus.lind@edu.gbssg.ch>
2024-09-16 09:26:56 +0200

    Tag 3 + API

---
+++ Tag 3/api.php
@@ -0,0 +1,14 @@
+<?php
+ 
+//api.php ? feld1=Hans & feld2=Meier
+ 
+ 
+echo '<pre>';
+echo '<h1> GET - Daten </h1> <hr>';
+ 
+print_r($_GET);
+ 
+echo '<h1> POST - Daten </h1> <hr>';
+ 
+print_r($_POST);
+ 
